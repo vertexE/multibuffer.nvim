db7fe03e043ff4df491ca13438955e43ef30bf3f
Josiah <44758384+vertexE@users.noreply.github.com>
2025-10-13 18:57:14 -0400

    fix: grep search (#4)
    * chore: debug print
    
    * chore: more vim.print
    
    * chore: add another grep option
    
    * fix: rg inputs for vim.system
    
    * chore: swap default to .
    
    * chore: title
    
    * chore: escaping changes
    
    * chore: always bufload
    
    * fix: correct path
    
    * fix: use absolute path
    
    * fix: off by 1 indexing
    
    * fix: ensure empty is not parsed
    
    * chore: cleanup vim.prints

--- lua/multibuffer/sources/grep.lua
+++ lua/multibuffer/sources/grep.lua
@@ -10,15 +10,17 @@ local parse_rg_line = function(i, line)
 	local segments = vim.split(line, ":")
 	-- local msg = table.concat(segments, ":", 4)
 	local path = segments[1]
-	local lnum = segments[2] and tonumber(segments[2]) or nil
-	local col = segments[3] and tonumber(segments[3]) or nil
+	path = path:gsub("^%./", "")
+	path = vim.fn.fnamemodify(path, ":p")
+	local lnum = segments[2] and (tonumber(segments[2]) - 1) or nil
+	local col = segments[3] and (tonumber(segments[3]) - 1) or nil
 	return { index = i, bufnr = -1, fp = path, lnum = lnum, col = col, lazy = true }
 end
 
 --- @param on_load fun(entries: table<multibuffer.Entry>)
 --- @param use_previous ?boolean
 M.search = function(on_load, use_previous)
-	vim.ui.input({}, function(input)
+	vim.ui.input({ prompt = "grep" }, function(input)
 		if not input or #input == 0 then
 			on_load({})
 			return
@@ -26,13 +28,14 @@ M.search = function(on_load, use_previous)
 
 		local result = vim.system({
 			"rg",
+			"--vimgrep",
 			"--glob",
-			"'!node_modules/**'",
+			"!node_modules/**",
 			input,
-			use_previous and table.concat(previous_entry_filepaths, " ") or "*",
-		}):wait()
+			use_previous and table.concat(previous_entry_filepaths, " ") or ".",
+		}, { text = true }):wait()
 		if result.stdout then
-			local lines = vim.split(result.stdout, "\n")
+			local lines = vim.split(result.stdout, "\n", { trimempty = true })
 			local entries = {}
 			for i, line in ipairs(lines) do
 				table.insert(entries, parse_rg_line(i, line))
