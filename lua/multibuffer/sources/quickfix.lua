c3b9c88cb7356b19496a88aa2b95f8dccaa8683e
Josiah Denton <jfranklindenton@gmail.com>
2025-10-12 21:18:34 -0400

    fix: index not i

--- lua/multibuffer/sources/quickfix.lua
+++ lua/multibuffer/sources/quickfix.lua
@@ -4,17 +4,19 @@ local M = {}
 M.quickfix_entries = function()
 	local quickfix_list = vim.fn.getqflist()
 	local entries = {}
-	for i, item in ipairs(quickfix_list) do
+	local i = 1
+	for _, item in ipairs(quickfix_list) do
 		if item.bufnr then
 			local name = vim.api.nvim_buf_get_name(item.bufnr)
 			local path = vim.fn.fnamemodify(name, ":~:.")
 			table.insert(entries, {
-				id = i,
+				index = i,
 				bufnr = item.bufnr,
 				lnum = item.lnum - 1, -- lnum is 1-based, convert to 0-based
 				msg = item.text,
 				fp = path,
 			})
+			i = i + 1
 		end
 	end
 
