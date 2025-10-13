7e91225240d52e84c379d4d0d99e87d8f35c039c
Josiah Denton <jfranklindenton@gmail.com>
2025-10-12 21:16:25 -0400

    fix: add id to entries for qfx

--- lua/multibuffer/sources/quickfix.lua
+++ lua/multibuffer/sources/quickfix.lua
@@ -4,11 +4,12 @@ local M = {}
 M.quickfix_entries = function()
 	local quickfix_list = vim.fn.getqflist()
 	local entries = {}
-	for _, item in ipairs(quickfix_list) do
+	for i, item in ipairs(quickfix_list) do
 		if item.bufnr then
 			local name = vim.api.nvim_buf_get_name(item.bufnr)
 			local path = vim.fn.fnamemodify(name, ":~:.")
 			table.insert(entries, {
+				id = i,
 				bufnr = item.bufnr,
 				lnum = item.lnum - 1, -- lnum is 1-based, convert to 0-based
 				msg = item.text,
